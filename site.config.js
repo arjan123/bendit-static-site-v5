1206e70bac97beda8cb08b70dbe6b66c9bfccc84
Arjan van Dijk <arjan@blacksnow.nl>
2026-07-09 00:27:34 +0200

    fix

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 window.BENDIT_CONFIG = {
   productName: "Bendit",
   siteUrl: "https://bend-it.net",
-  assetVersion: "20260712",
+  assetVersion: "20260714",
 
   // Self-hosted build. Place the built zip at /downloads/Bendit-Windows-x64.zip
   // in the repo. Keep the filename identical across releases so this link
